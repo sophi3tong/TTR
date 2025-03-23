27064e4dceb8a44faf7c93d14a623275c874e564
samchang1121 <151976103+samchang1121@users.noreply.github.com>
2025-03-23 14:08:51 -0400

    trying audio

--- levelwindow.h
+++ levelwindow.h
@@ -7,12 +7,19 @@
 #include <QLineEdit>
 #include <QPushButton>
 #include <QCoreApplication>
+#include <SFML/Audio.hpp>
+#include <QListWidget>
 
 class LevelWindow : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit LevelWindow(QWidget *parent = nullptr);
+    void playPreview(QPushButton *item);
+    void playFullSong(QPushButton *item);
+    void pausePlayback();
+    void stopPlayback();
+    void restartPlayback();
 
 private:
     QVBoxLayout *layout;
@@ -20,6 +27,13 @@ private:
     QPushButton *levelButton1;
     QPushButton *levelButton2;
     QPushButton *levelButton3;
+    QPushButton *pauseButton;
+    QPushButton *stopButton;
+    QPushButton *restartButton;
+    QMap<QPushButton*, QString> songPaths;
+    QListWidget *songList;
+    QString currentSongPath;
+    sf::Music musicPlayer;
 };
 
 #endif // LEVELWINDOW_H
